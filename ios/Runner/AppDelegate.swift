c09f67f45e41e74f00a8217db9c9c7fce27af922
amiirrallii <amirali007z@yahoo.com>
2024-06-29 14:12:03 +0330

    Creating a Connection between Flutter and iOS Native by adding codes in AppDelegate.swift.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,18 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+    let incrementChannel = FlutterMethodChannel(name: "com.filio.increment",
+                                                binaryMessenger: controller.binaryMessenger)
+    incrementChannel.setMethodCallHandler { (call: FlutterMethodCall, result: @escaping FlutterResult) in
+        if call.method == "incrementCounter" {
+            DatabaseHelper.shared.incrementCounter()
+            result("Counter incremented")
+        } else {
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
